55e21f27dd39d5aa5e74763795083fbfae08e45d
Louison Parant <louison.parant@etu.univ-lyon1.fr>
2025-01-07 16:16:24 +0100

    pour pas faire de merge nul

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const { createApp, ref } = Vue
 
+const playingTrack = ref('Choose a track to play.')
+const trackList = ref(['Add a new track to the playlist.', 'Venom'])
+
 createApp({
   setup() {
-    const playingTrack = ref('Choose a track to play.')
-    const trackList = ref(['Add a new track to the playlist.', 'Venom'])
     return {
         playingTrack,
         trackList
     }
   }
 }).mount('#app')
+
+function addTrackByURL() {
+    const url = document.getElementById('urlTrack')
+    trackList.value.push(url.innerText)
+}
