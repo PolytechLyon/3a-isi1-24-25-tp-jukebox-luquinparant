2e325f5d7c57d7a94f2e9557bbdb99bc7f0e8826
Louison Parant <louison.parant@etu.univ-lyon1.fr>
2025-01-07 16:30:28 +0100

    c'est tout dans le setup

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const { createApp, ref } = Vue
 
-const playingTrack = ref('Choose a track to play.')
-const trackList = ref(['Add a new track to the playlist.', 'Venom'])
-
 createApp({
   setup() {
+    const playingTrack = ref('Choose a track to play.')
+    const trackList = ref(['Add a new track to the playlist.'])
+
     function addTrackByURL() {
         const url = document.getElementById('urlTrack')
         trackList.value.push(url.value)
         url.value = ""
     }
+    
     return {
         playingTrack,
         trackList,
