2560fa55f07b5d28a6863a7ab104e25b8038bdc9
Louison Parant <louison.parant@etu.univ-lyon1.fr>
2025-01-16 11:01:26 +0100

    add track Managing

--- jukebox/src/components/Home.vue
+++ jukebox/src/components/Home.vue
@@ -1,61 +1,71 @@
 <script setup>
-import { createApp, ref } from 'vue'
-    const playingTrack = ref('Choose a track to play.')
+    import { ref } from 'vue'
+    const isPlayingTrack = ref(false)
+    const trackPlayed = ref("")
     const trackList = ref([])
 
-    function addTrackByURL() {
-      const url = document.getElementById('urlTrack')
-      trackList.value.push(url.value);
-      url.value = ""
+    async function addTrackByURL() {
+        const url = document.getElementById('urlTrack')
+        trackList.value.push(url.value)
+        url.value = ""
     }
 
     const playTrack = (index) => {
-      console.log(index)
-      playingTrack.value = trackList.value[index]
+        console.log(index)
+        isPlayingTrack.value = !isPlayingTrack.value
+        trackPlayed.value = trackList.value[index]
     }
 
     const deleteTrack = (index) => {
-      console.log(index)
-      trackList.value.splice(index, 1)
+        trackList.value.splice(index, 1);
     }
 </script>
 
 <template>
     <h1>Jukebox</h1>
     <section>
-      <h2>Player</h2>
-      <div>{{ playingTrack }}</div>
-      <fieldset>
-        <legend>Playback Mode</legend>
-        <input name="Playback" id="repeatList" type="radio" value="REPEAT_LIST" checked>
-        <label for="repeatList">Repeat List</label>
-        <input name="Playback" id="repeatTrack" type="radio" value="REPEAT_TRACK">
-        <label for="repeatTrack">Repeat Track</label>
-        <input name="Playback" id="doNotRepeat" type="radio" value="DO_NOT_REPEAT">
-        <label for="doNotRepeat">Don&apos;t Repeat</label>
-      </fieldset>
+        <h2>Player</h2>
+        <div v-if="!isPlayingTrack">
+            <p>Choose a track to play.</p>
+        </div>
+        <div v-else>
+            <p>Playing: {{ trackPlayed }}</p>
+            <audio>
+                <source src="" type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
+        </div>
+        <fieldset>
+            <legend>Playback Mode</legend>
+            <input name="Playback" id="repeatList" type="radio" value="REPEAT_LIST" checked>
+            <label for="repeatList">Repeat List</label>
+            <input name="Playback" id="repeatTrack" type="radio" value="REPEAT_TRACK">
+            <label for="repeatTrack">Repeat Track</label>
+            <input name="Playback" id="doNotRepeat" type="radio" value="DO_NOT_REPEAT">
+            <label for="doNotRepeat">Don&apos;t Repeat</label>
+        </fieldset>
     </section>
     <section>
-      <h2>Playlist</h2>
-      <ul>
-        <li v-for="track in trackList" :key="track">
-          <span>{{ track }}</span>
-          <button role="button" @click="playTrack">Play</button>
-          <button role="button" @click="deleteTrack">Delete</button>
-        </li>
-      </ul>
+        <h2>Playlist</h2>
+        <ul>
+            <li v-for="track in trackList" :key="track">
+                <span>{{ track }}</span>
+                <button role="button" @click="playTrack(trackList.indexOf(track))">Play</button>
+                <button role="button" @click="deleteTrack(trackList.indexOf(track))">Delete</button>
+            </li>
+        </ul>
     </section>
     <section>
-      <h2>New Track</h2>
-      <div>
-        <label for="addTrackSelector">Add Track</label>
-        <select id="addTrackSelector">
-          <option value="url">By URL</option>
-          <option value="upload">Via file upload</option>
-        </select>
-        <input placeholder="Provide URL" type="text" id="urlTrack">
-        <button @click="addTrackByURL()">Add by URL</button>
-      </div>
+        <h2>New Track</h2>
+        <div>
+            <label for="addTrackSelector">Add Track</label>
+            <select id="addTrackSelector">
+                <option value="url">By URL</option>
+                <option value="upload">Via file upload</option>
+            </select>
+            <input placeholder="Provide URL" type="text" id="urlTrack">
+            <button @click="addTrackByURL()">Add by URL</button>
+        </div>
     </section>
 </template>
 
