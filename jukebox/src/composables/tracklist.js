eb0c5f9a06eec4c5115d039a5667a22129dc34ae
Luquin Emmanuel p2202699 <emmanuel.luquin@etu.univ-lyon1.fr>
2025-01-17 12:45:37 +0100

    Fixed play buttons

--- jukebox/src/composables/tracklist.js
+++ jukebox/src/composables/tracklist.js
@@ -1,4 +1,4 @@
-import { ref, onMounted, onUnmounted, nextTick } from 'vue'
+import {ref, onMounted, onUnmounted, nextTick} from 'vue'
 
 export function useTracklist() {
 
@@ -19,15 +19,27 @@ export function useTracklist() {
         isPlayingTrack.value = true
         trackPlayed.value = index
         textPlayable.value = "Pause"
-        audioRef.value.play()
+        audioRef.value.currentTime = 0
+        nextTick(() => (audioRef.value.play()))
     }
 
     const deleteTrack = (index) => {
         trackList.value.splice(index, 1)
+        if (trackList.value.length === 0) {
+            isPlayingTrack.value = false;
+        }
     }
 
     const updateProgress = () => {
         progressRef.value.value = (100 * audioRef.value.currentTime) / audioRef.value.duration
+    }
+
+    //marche pas
+    const progressClick = (event) => {
+        progressRef.value.value = Math.min(Math.max((event.offsetX / progressRef.value.width) * 100, 0), 100)
+        audioRef.value.pause()
+        audioRef.value.currentTime = audioRef.value.currentTime * (progressRef.value.value / 100)
+        audioRef.value.play()
     }
 
     const playPause = () => {
@@ -42,7 +54,9 @@ export function useTracklist() {
     }
 
     onMounted(() => audioRef.value.addEventListener('timeupdate', updateProgress));
+    //onMounted(() => progressRef.value.addEventListener('click', progressClick));
     onUnmounted(() => audioRef.value.removeEventListener('timeupdate', updateProgress));
+    //onUnmounted(() => progressRef.value.removeEventListener('click', progressClick));
 
     return { trackList, addTrackByURL, deleteTrack, isPlayingTrack, trackPlayed, playTrack, playPause, textPlayable, audioRef, progressRef }
 }
